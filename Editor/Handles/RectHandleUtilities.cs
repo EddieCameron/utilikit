8c69febbb30df710f4133c85bc747d7046f9dee9
Eddie Cameron <eddiec@grapefruitgames.com>
2019-10-26 15:11:33 -0400

    - distribute in rect tool

---
+++ Editor/Handles/RectHandleUtilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace Utilikit {
+    public static class RectHandleUtilities {
+        public static Rect DrawRectHandles( Rect inRect, float worldZ, Handles.CapFunction capFunc, Color capCol, Color fillCol, float capSize, float snap ) {
+            
+            Vector3[] corners = { new Vector2( inRect.xMin, inRect.yMax ),    // Top Left
+                                   inRect.max,     // Top Right
+                                   new Vector2( inRect.xMax, inRect.yMin ),    // Bottom Right
+                                   inRect.min }; // Bottom Left
+            for ( int i = 0; i < 4; i++ ) {
+                corners[i].z = worldZ;
+            }
+
+            Handles.DrawSolidRectangleWithOutline( corners, new Color( fillCol.r, fillCol.g, fillCol.b, 0.25f ), capCol );
+
+
+            // Calculate the 4 control points. Should be the center of each edge.            
+            Vector3[] handlePoints =  { new Vector2( inRect.xMin, inRect.center.y ),   // Left
+                                         new Vector2( inRect.xMax, inRect.center.y ),    // Right
+                                         new Vector2( inRect.center.x, inRect.yMax ),    // Top
+                                         new Vector2( inRect.center.x, inRect.yMin ) }; // Bottom 
+
+            Handles.color = capCol;
+            handlePoints[0] = Handles.Slider( handlePoints[0], -Vector3.right, capSize, capFunc, snap ); // Left Handle
+            handlePoints[1] = Handles.Slider( handlePoints[1], Vector3.right, capSize, capFunc, snap );  // Right Handle
+            handlePoints[2] = Handles.Slider( handlePoints[2], Vector3.up, capSize, capFunc, snap );     // Top Handle
+            handlePoints[3] = Handles.Slider( handlePoints[3], -Vector3.up, capSize, capFunc, snap );    // Bottom Handle
+
+            Vector2 min = new Vector2( handlePoints[0].x, handlePoints[3].y );
+            Vector2 size = new Vector2( handlePoints[1].x - handlePoints[0].x,
+                handlePoints[2].y - handlePoints[3].y );
+
+            return new Rect( min, size );
+        }
+    }
+}
---
+++ Editor/Tools/DistributeInRect.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+using UnityEditor;
+using UnityEditor.EditorTools;
+
+namespace Utilikit {
+    [EditorTool( "Distribute In Rect" )]
+    public class DistributeInRect : EditorTool {
+        private Rect rect;
+        private float minDist;
+        private float maxDist;
+        private GameObject currentSelection;
+
+        public override bool IsAvailable() {
+            return Selection.gameObjects != null && Selection.gameObjects.Length == 1;
+        }
+
+        public override void OnToolGUI( EditorWindow window ) {
+            if ( Selection.activeGameObject == null )
+                return;
+
+            if ( currentSelection != Selection.activeGameObject ) {
+                // reset
+                currentSelection = Selection.activeGameObject;
+                if ( rect.size.magnitude < .1f ) {
+                    rect.size = new Vector2( 10, 10 );
+                }
+                rect.center = Selection.activeGameObject.transform.position;
+            }
+
+            Color rectColor = new Color( .52f, .76f, .86f );
+            rect = RectHandleUtilities.DrawRectHandles( rect, currentSelection.transform.position.z, Handles.DotHandleCap, rectColor, Color.clear, 0.1F * HandleUtility.GetHandleSize( currentSelection.transform.position ), 0f );
+
+            Handles.BeginGUI();
+            using ( var area = new GUILayout.AreaScope( new Rect( 20, 20, 300f, 200f ) ) ) {
+                using ( var vert = new GUILayout.VerticalScope() ) {
+                    minDist = EditorGUILayout.FloatField( "Min Dist", minDist );
+                    maxDist = EditorGUILayout.FloatField( "Max Dist", maxDist );
+
+                    if ( GUILayout.Button( "Distribute!" ) ) {
+                        var dist = new PoissonDiscSampler( rect.width, rect.height, minDist, maxDist );
+                        int undoId = Undo.GetCurrentGroup();
+                        foreach (var point in dist.Samples() )
+                        {
+                            Vector3 pos = rect.min + point;
+                            pos.z = currentSelection.transform.position.z;
+
+                            GameObject newObj;
+                            if ( PrefabUtility.IsAnyPrefabInstanceRoot( currentSelection ) ) {
+                                var prefab = PrefabUtility.GetCorrespondingObjectFromSource( currentSelection );
+                                newObj = (GameObject)PrefabUtility.InstantiatePrefab( prefab, currentSelection.transform.parent );
+                                newObj.transform.position = pos;
+                            }
+                            else {
+                                newObj = Instantiate( currentSelection, pos, currentSelection.transform.rotation, currentSelection.transform.parent );
+                            }
+                            Undo.RegisterCreatedObjectUndo( newObj, "Ditribute Object" );
+                            Undo.CollapseUndoOperations( undoId );
+                        }
+                    }
+                }
+            }
+            Handles.EndGUI();
+        }
+    }
+}
