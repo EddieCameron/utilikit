9b91c7c4d50a355d0b721b688e3878caa4757c1e
Eddie Cameron <eddiec@grapefruitgames.com>
2019-11-02 11:50:58 -0400

    - sprite or image reference

---
+++ Editor/Inspector/SpriteOrImageDrawer.cs
@@ -0,0 +1,71 @@
+﻿/* SpriteOrImageDrawer.cs
+ * © Eddie Cameron 2019
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using Utilikit;
+using Random = UnityEngine.Random;
+
+namespace Utilikit {
+    [CustomPropertyDrawer( typeof( SpriteOrImage ))]
+    public class SpriteOrImageDrawer : PropertyDrawer {
+
+        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label ) {
+            SerializedProperty spriteProp = property.FindPropertyRelative( "_spriteRenderer" );
+            SerializedProperty imageProp = property.FindPropertyRelative( "_image" );
+
+            bool isImage = imageProp.objectReferenceValue != null;
+            UnityEngine.Object referencedObject = isImage ? imageProp.objectReferenceValue : spriteProp.objectReferenceValue;
+
+            var draggedObject = EditorGUI.ObjectField( position, label, referencedObject, typeof( Component ), allowSceneObjects: true );
+            if ( draggedObject != referencedObject ) {
+                Component draggedComponent = GetImageOrSpriteFromDraggedObject( draggedObject );
+                
+                if ( draggedComponent is Image ) {
+                    spriteProp.objectReferenceValue = null;
+                    imageProp.objectReferenceValue = draggedComponent;
+                }
+                else if ( draggedComponent is SpriteRenderer ) {
+                    spriteProp.objectReferenceValue = draggedComponent;
+                    imageProp.objectReferenceValue = null;
+                }
+                else {
+                    spriteProp.objectReferenceValue = null;
+                    imageProp.objectReferenceValue = null;
+                }
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        Component GetImageOrSpriteFromDraggedObject( UnityEngine.Object draggedObject ) {
+            if ( draggedObject == null )
+                return null;
+
+            if ( draggedObject is Image i )
+                return i;
+            if ( draggedObject is SpriteRenderer s )
+                return s;
+
+            if ( draggedObject is Component c ) {
+                // find a valid image/sprite on same gameobject
+                SpriteRenderer sprite = c.GetComponent<SpriteRenderer>();
+                if ( sprite != null )
+                    return sprite;
+
+                Image image = c.GetComponent<Image>();
+                if ( image != null )
+                    return image;
+            }
+
+            return null;    // no valid object
+        }
+    }
+}
---
+++ Runtime/Inspector/SpriteOrImage.cs
@@ -0,0 +1,82 @@
+﻿/* SpriteOrImage.cs
+ * © Eddie Cameron 2019
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+
+namespace Utilikit {
+    [Serializable]
+    public class SpriteOrImage {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+        public SpriteRenderer SpriteRenderer => _spriteRenderer;
+
+        [SerializeField]
+        private Image _image;
+        public Image Image => _image;
+
+        public bool IsSpriteRenderer => _spriteRenderer != null;
+        public bool IsImage => _image != null;
+
+        public bool HasReference => _spriteRenderer != null || _image != null;
+        public Component ReferencedComponent => IsSpriteRenderer ? (Component)_spriteRenderer : (Component)_image;
+
+        public bool Enabled {
+            get {
+                if ( IsSpriteRenderer )
+                    return _spriteRenderer.enabled;
+                else if ( IsImage )
+                    return _image.enabled;
+                else
+                    throw new NullReferenceException( "No sprite renderer or image defined" );
+            }
+            set {
+                if ( IsSpriteRenderer )
+                    _spriteRenderer.enabled = value;
+                else if ( IsImage )
+                    _image.enabled = value;
+                else
+                    throw new NullReferenceException( "No sprite renderer or image defined" );
+            }
+        }
+
+        public Sprite Sprite {
+            get {
+                return _spriteRenderer?.sprite ?? _image?.sprite;
+            }
+            set {
+                if ( IsSpriteRenderer )
+                    _spriteRenderer.sprite = value;
+                else if ( IsImage )
+                    _image.sprite = value;
+                else
+                    throw new NullReferenceException( "No sprite renderer or image defined" );
+            }
+        }
+
+        public Color Color {
+            get {
+                if ( IsSpriteRenderer )
+                    return _spriteRenderer.color;
+                else if ( IsImage )
+                    return _image.color;
+                else
+                    throw new NullReferenceException( "No sprite renderer or image defined" );
+            }
+            set {
+                if ( IsSpriteRenderer )
+                    _spriteRenderer.color = value;
+                else if ( IsImage )
+                    _image.color = value;
+                else
+                    throw new NullReferenceException( "No sprite renderer or image defined" );
+            }
+        }
+    }
+}
