9dbc455f231d23f81d960f66b100ea39a6715e9b
Eddie Cameron <eddiec@grapefruitgames.com>
2019-11-12 12:00:43 -0500

    - drag button

---
+++ Runtime/DebugExtensions.cs
@@ -0,0 +1,45 @@
+﻿/* DebugExtensions.cs
+ * © Eddie Cameron 2019
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+
+namespace Utilikit {
+    public static class DebugExtensions {
+
+
+        /// <summary>
+        /// Draw a rect in the scene
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="zPos"></param>
+        public static void DrawRect( Rect rect, float zPos = 0 ) {
+            DrawRect( rect, zPos, Color.white, 0 );
+        }
+
+        /// <summary>
+        /// Draw a rect in the scene
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="zPos"></param>
+        public static void DrawRect( Rect rect, float zPos, Color c, float t = 0 ) {
+            Vector3 min = rect.min;
+            min.z = zPos;
+            Vector3 max = rect.max;
+            max.z = zPos;
+
+            Vector3 upperLeft = new Vector3( min.x, max.y, zPos );
+            Vector3 lowerRight = new Vector3( max.x, min.y, zPos );
+            Debug.DrawLine( min, upperLeft, c, t );
+            Debug.DrawLine( upperLeft, max, c, t );
+            Debug.DrawLine( max, lowerRight, c, t );
+            Debug.DrawLine( lowerRight, min, c, t );
+        }
+    }
+}
---
+++ Runtime/UI/DragButton.cs
@@ -0,0 +1,56 @@
+﻿/* DragButton.cs
+ * © Eddie Cameron 2019
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+
+namespace Utilikit {
+    /// <summary>
+    /// Just a UI object that sends events when dragged from
+    /// </summary>
+    public class DragButton : Selectable, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+        public class DragButtonEvent : UnityEvent<PointerEventData> { }
+
+        [SerializeField]
+        private DragButtonEvent _onBeginDrag = new DragButtonEvent();
+        public DragButtonEvent OnBeginDrag => _onBeginDrag;
+        
+        [SerializeField]
+        private DragButtonEvent _onDrag = new DragButtonEvent();
+        public DragButtonEvent OnDrag => _onDrag;
+        
+        [SerializeField]
+        private DragButtonEvent _onEndDrag = new DragButtonEvent();
+        public DragButtonEvent OnEndDrag => _onEndDrag;
+
+        void IBeginDragHandler.OnBeginDrag( PointerEventData eventData ) {
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            _onBeginDrag.Invoke( eventData );
+        }
+
+        void IDragHandler.OnDrag( PointerEventData eventData ) {
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            _onDrag.Invoke( eventData );
+        }
+
+        void IEndDragHandler.OnEndDrag( PointerEventData eventData ) {
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            _onEndDrag.Invoke( eventData );
+        }
+    }
+}
