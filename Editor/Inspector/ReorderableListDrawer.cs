199d5e22c4d4f27ba7c7aff42d99d243d5de0925
Eddie Cameron <eddiec@grapefruitgames.com>
2020-06-17 13:20:35 -0400

    namespaces

--- Editor/Inspector/ReorderableListDrawer.cs
+++ Editor/Inspector/ReorderableListDrawer.cs
@@ -7,53 +7,53 @@ using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 
-[CustomPropertyDrawer( typeof( ReorderableList<> ), useForChildren: true )]
-public class ReorderableListDrawer : PropertyDrawer {
+namespace Utilikit {
+    [CustomPropertyDrawer( typeof( ReorderableList<> ), useForChildren: true )]
+    public class ReorderableListDrawer : PropertyDrawer {
 
-    Dictionary<string, ReorderableList> _listsPerProp = new Dictionary<string, ReorderableList>();
+        Dictionary<string, ReorderableList> _listsPerProp = new Dictionary<string, ReorderableList>();
 
-    ReorderableList GetReorderableList( SerializedProperty prop ) {
-        SerializedProperty listProperty = prop.FindPropertyRelative( "_list" );
+        ReorderableList GetReorderableList( SerializedProperty prop ) {
+            SerializedProperty listProperty = prop.FindPropertyRelative( "_list" );
 
-        ReorderableList list;
-        if ( _listsPerProp.TryGetValue( listProperty.propertyPath, out list ) ) {
+            ReorderableList list;
+            if ( _listsPerProp.TryGetValue( listProperty.propertyPath, out list ) ) {
+                return list;
+            }
+
+            list = new ReorderableList( listProperty.serializedObject, listProperty, draggable: true, displayHeader: true, displayAddButton: true, displayRemoveButton: true );
+            _listsPerProp[listProperty.propertyPath] = list;
+
+            list.drawHeaderCallback += rect => {
+                EditorGUI.LabelField( rect, prop.displayName );
+            };
+
+            list.drawElementCallback += ( rect, index, isActive, isFocused ) => {
+                SerializedProperty elementProp = list.serializedProperty.GetArrayElementAtIndex( index );
+                if ( elementProp.hasVisibleChildren ) {
+                    EditorGUI.PropertyField( rect, elementProp, includeChildren: true );
+                }
+                else {
+                    EditorGUI.PropertyField( rect, elementProp, includeChildren: true, label: GUIContent.none );   // dont draw label if its a single line
+                }
+            };
+
+            list.elementHeightCallback += idx => {
+                SerializedProperty elementProp = listProperty.GetArrayElementAtIndex( idx );
+                return EditorGUI.GetPropertyHeight( elementProp );
+            };
+
             return list;
         }
 
-        list = new ReorderableList( listProperty.serializedObject, listProperty, draggable: true, displayHeader: true, displayAddButton: true, displayRemoveButton: true );
-        _listsPerProp[listProperty.propertyPath] = list;
+        public override void OnGUI( Rect rect, SerializedProperty serializedProperty, GUIContent label ) {
+            ReorderableList list = GetReorderableList( serializedProperty );
 
-        list.drawHeaderCallback += rect => {
-            EditorGUI.LabelField( rect, prop.displayName );
-        };
+            list.DoList( rect );
+        }
 
-        list.drawElementCallback += ( rect, index, isActive, isFocused ) => {
-            SerializedProperty elementProp = list.serializedProperty.GetArrayElementAtIndex( index );
-            if ( elementProp.hasVisibleChildren ) {
-                EditorGUI.PropertyField( rect, elementProp, includeChildren: true );
-            }
-            else {
-                EditorGUI.PropertyField( rect, elementProp, includeChildren: true, label: GUIContent.none );   // dont draw label if its a single line
-            }
-        };
-
-        list.elementHeightCallback += idx => {
-            SerializedProperty elementProp = listProperty.GetArrayElementAtIndex( idx );
-            return EditorGUI.GetPropertyHeight( elementProp );
-        };
-
-        return list;
-    }
-
-    public override void OnGUI( Rect rect, SerializedProperty serializedProperty, GUIContent label )
-    {
-        ReorderableList list = GetReorderableList( serializedProperty );
-
-        list.DoList( rect );
-    }
-
-    public override float GetPropertyHeight( SerializedProperty serializedProperty, GUIContent label )
-    {
-        return GetReorderableList( serializedProperty ).GetHeight();
+        public override float GetPropertyHeight( SerializedProperty serializedProperty, GUIContent label ) {
+            return GetReorderableList( serializedProperty ).GetHeight();
+        }
     }
 }
--- Runtime/Inspector/ReorderableList.cs
+++ Runtime/Inspector/ReorderableList.cs
@@ -9,54 +9,56 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-[Serializable]
-public abstract class ReorderableList<T> : IList<T> {
-    [SerializeField]
-    private List<T> _list;
+namespace Utilikit {
+    [Serializable]
+    public abstract class ReorderableList<T> : IList<T> {
+        [SerializeField]
+        private List<T> _list;
 
-    public T this[int index] { get => _list[index]; set => _list[index] = value; }
+        public T this[int index] { get => _list[index]; set => _list[index] = value; }
 
-    public int Count => _list.Count;
+        public int Count => _list.Count;
 
-    public bool IsReadOnly => false;
+        public bool IsReadOnly => false;
 
-    public void Add( T item ) {
-        _list.Add( item );
-    }
+        public void Add( T item ) {
+            _list.Add( item );
+        }
 
-    public void Clear() {
-        _list.Clear();
-    }
+        public void Clear() {
+            _list.Clear();
+        }
 
-    public bool Contains( T item ) {
-        return _list.Contains( item );
-    }
+        public bool Contains( T item ) {
+            return _list.Contains( item );
+        }
 
-    public void CopyTo( T[] array, int arrayIndex ) {
-        _list.CopyTo( array, arrayIndex );
-    }
+        public void CopyTo( T[] array, int arrayIndex ) {
+            _list.CopyTo( array, arrayIndex );
+        }
 
-    public IEnumerator<T> GetEnumerator() {
-        return _list.GetEnumerator();
-    }
+        public IEnumerator<T> GetEnumerator() {
+            return _list.GetEnumerator();
+        }
 
-    public int IndexOf( T item ) {
-        return _list.IndexOf( item );
-    }
+        public int IndexOf( T item ) {
+            return _list.IndexOf( item );
+        }
 
-    public void Insert( int index, T item ) {
-        _list.Insert( index, item );
-    }
+        public void Insert( int index, T item ) {
+            _list.Insert( index, item );
+        }
 
-    public bool Remove( T item ) {
-        return _list.Remove( item );
-    }
+        public bool Remove( T item ) {
+            return _list.Remove( item );
+        }
 
-    public void RemoveAt( int index ) {
-        _list.RemoveAt( index );
-    }
+        public void RemoveAt( int index ) {
+            _list.RemoveAt( index );
+        }
 
-    IEnumerator IEnumerable.GetEnumerator() {
-        return _list.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() {
+            return _list.GetEnumerator();
+        }
     }
 }
