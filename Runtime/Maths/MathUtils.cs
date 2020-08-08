69a0d1c936dc749e4e2ca2ab8713a0fd31c0b438
Eddie Cameron <eddiec@grapefruitgames.com>
2020-08-08 14:09:58 -0400

    bounds rect

--- Runtime/Maths/MathUtils.cs
+++ Runtime/Maths/MathUtils.cs
@@ -11,6 +11,35 @@ namespace Utilikit {
         /// <param name="rect"></param>
         /// <param name="point"></param>
         /// <returns></returns>
+        public static Rect GetBoundingRect( IEnumerable<Vector2> points ) {
+            var e = points.GetEnumerator();
+            if ( !e.MoveNext() )
+                return default( Rect );
+
+            Rect rect = new Rect( e.Current, Vector2.zero );
+            while ( e.MoveNext() ) {
+                var point = e.Current;
+
+                if ( point.x < rect.xMin )
+                    rect.xMin = point.x;
+                else if ( point.x > rect.xMax )
+                    rect.xMax = point.x;
+
+                if ( point.y < rect.yMin )
+                    rect.yMin = point.y;
+                else if ( point.y > rect.yMax )
+                    rect.yMax = point.y;
+            }
+
+            return rect;
+        }
+
+        /// <summary>
+        /// Create a new Rect that expands this rect to include the given point
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="point"></param>
+        /// <returns></returns>
         public static Rect Encapsulate( this Rect rect, Vector2 point ) {
             if ( point.x < rect.xMin )
                 rect.xMin = point.x;
