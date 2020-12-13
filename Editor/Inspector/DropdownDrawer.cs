34aa2778c0fb4e07906e675c7a2254a1560a6413
Eddie Cameron <eddiec@grapefruitgames.com>
2020-12-13 18:08:43 +1300

    - string dropdown for inspector

---
+++ Editor/Inspector/DropdownDrawer.cs
@@ -0,0 +1,77 @@
+﻿/* DropdownDrawer.cs
+ * © Eddie Cameron 2021
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+using UnityEditor;
+using System.Linq;
+
+[CustomPropertyDrawer( typeof( DropdownAttribute ))]
+public class DropdownDrawer : PropertyDrawer {
+
+    GUIContent[] dropdownOptions;
+
+    void GetDropdownOptions() {
+        DropdownAttribute dropdownAttr = (DropdownAttribute)attribute;
+        Type staticType;
+        if ( string.IsNullOrEmpty( dropdownAttr.staticMethodType ) ) {
+            staticType = fieldInfo.DeclaringType;
+        }
+        else {
+            staticType = fieldInfo.DeclaringType.Assembly.GetType( dropdownAttr.staticMethodType );
+            if ( staticType == null ) {
+                Debug.LogError( "Get dropdown options type not found: " + dropdownAttr.staticMethodType );
+                return;
+            }
+        }
+
+        var getOptionsMethod = staticType.GetMethod( dropdownAttr.staticMethodName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public );
+        if ( getOptionsMethod == null ) {
+            Debug.LogError( "Get dropdown options method not found: " + dropdownAttr.staticMethodName );
+            return;
+        }
+
+        var optionsEnumerable = getOptionsMethod.Invoke( null, null ) as IEnumerable<string>;
+        if ( optionsEnumerable == null ) {
+            Debug.LogError( "Get dropdown options method doesn't return an enumerable string collection" );
+            return;
+        }
+        dropdownOptions = optionsEnumerable.Select( s => new GUIContent( s ) ).ToArray();
+    }
+
+    public override void OnGUI( Rect position, SerializedProperty property, GUIContent label ) {
+        if ( dropdownOptions == null ) {
+            GetDropdownOptions();
+        }
+
+        if ( dropdownOptions == null ) {
+            EditorGUI.PropertyField( position, property, label );
+            return;
+        }
+
+        int selectedOption = -1;
+        for ( int i = 0; i < dropdownOptions.Length; i++ ) {
+            if ( property.stringValue == dropdownOptions[i].text ) {
+                selectedOption = i;
+                break;
+            }
+        }
+
+        var startColour = GUI.contentColor;
+        if ( selectedOption == -1 ) {
+            // invalid
+            GUI.contentColor = Color.red;
+        }
+        selectedOption = EditorGUI.Popup( position, label, selectedOption, dropdownOptions );
+        if ( selectedOption >= 0 ) {
+            property.stringValue = dropdownOptions[selectedOption].text;
+        }
+        GUI.contentColor = startColour;
+    }
+}
---
+++ Runtime/Inspector/DropdownAttribute.cs
@@ -0,0 +1,32 @@
+﻿/* Dropdown.cs
+ * © Eddie Cameron 2021
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+
+/// <summary>
+/// Use on a string field to show a dropdown list of possible options
+/// Options are found by calling the static method in the attribute constructor, returning a collection of strings.
+/// If no type is given, static method will be called on the field's containing class
+/// </summary>
+public class DropdownAttribute : PropertyAttribute {
+
+    public string staticMethodType;
+    public string staticMethodName;
+
+    public DropdownAttribute( string getStringListStaticMethodType, string getStringListStaticMethodName ) {
+        this.staticMethodType = getStringListStaticMethodType;
+        this.staticMethodName = getStringListStaticMethodName;
+    }
+
+    public DropdownAttribute( string getStringListStaticMethodName ) {
+        this.staticMethodType = "";
+        this.staticMethodName = getStringListStaticMethodName;
+    }
+}
