8e873f43b257ca06a02b28e2b982c84bfa0dda7c
Eddie Cameron <eddiec@grapefruitgames.com>
2021-02-04 11:55:29 +1300

    - layer drawer

---
+++ Editor/Inspector/LayerDrawer.cs
@@ -0,0 +1,27 @@
+﻿/* SortingLayerDrawer.cs
+ * © Eddie Cameron 2021
+ * ----------------------------
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilikit;
+using Random = UnityEngine.Random;
+
+[CustomPropertyDrawer( typeof( LayerAttribute ) )]
+public class LayerDrawer : PropertyDrawer {
+
+    public override void OnGUI( Rect position, SerializedProperty property, GUIContent label ) {
+        if ( property.propertyType != SerializedPropertyType.Integer ) {
+            // Integer is expected. Everything else is ignored.
+            return;
+        }
+
+        using ( var scope = new EditorGUI.PropertyScope( position, label, property ) ) {
+            property.intValue = EditorGUI.LayerField( position, scope.content, property.intValue, EditorStyles.popup );
+        }
+    }
+}
